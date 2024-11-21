bd18058b723e5303d8bda6e59d9ad1055605f09c
karimmerhom <karimebrahim328@gmail.com>
2024-11-21 22:06:36 +0200

    fixing nav bar waiting docs

--- roboclaim-hiring-task-fe/src/components/nav-bar/nav-bar.jsx
+++ roboclaim-hiring-task-fe/src/components/nav-bar/nav-bar.jsx
@@ -1,6 +1,6 @@
-'use client';
-import React, { useContext, useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { Box, Flex, Text, IconButton } from "@chakra-ui/react";
 import { ThemeContext } from "@/context/theme";
 import { FaSignOutAlt } from "react-icons/fa";
@@ -9,32 +9,53 @@ import { signOut } from "@/api/auth";
 
 const NavBar = () => {
   const themCtx = useContext(ThemeContext);
-  const router = useRouter();
-  const [urlPart, setUrlPart] = useState('');
+  const pathname = usePathname();
+  const [urlPart, setUrlPart] = useState("");
 
   useEffect(() => {
-    const currentPath = window.location.pathname;
-    let lastPart = currentPath.split('/').pop();
-    lastPart = lastPart.charAt(0).toUpperCase() + lastPart.slice(1);
-    if (!isNaN(lastPart)) {
-      setUrlPart(`File ID: ${lastPart}`);
-    } else {
-      setUrlPart(lastPart);
+    if (typeof window !== "undefined" && pathname) {
+      const currentPath = pathname;
+      let lastPart = currentPath.split("/").pop();
+      lastPart = lastPart.charAt(0).toUpperCase() + lastPart.slice(1);
+      if (lastPart === "Portal") {
+        setUrlPart(lastPart);
+      } else {
+        setUrlPart(`File ID: ${lastPart}`);
+      }
     }
-  }, [router.asPath]);
+  }, [pathname]);
 
   return (
-    <Box bg="background.primary" px={5} boxShadow="secondaryShadow" borderBottomWidth={2} borderColor="border.secondaryColor">
+    <Box
+      bg="background.primary"
+      px={5}
+      boxShadow="secondaryShadow"
+      borderBottomWidth={2}
+      borderColor="border.secondaryColor"
+    >
       <Flex h={16} alignItems="center" justifyContent="space-between">
         <Text color="text.primary" fontSize={24} fontWeight="bold">
-          {urlPart || 'Dashboard'}
+          {urlPart || "Dashboard"}
         </Text>
         <Flex alignItems="center">
-          <Box display="flex" flexDirection="row" bg="background.primary" px={2} py={1} borderRadius={100} boxShadow="secondaryShadow" mr={4}>
+          <Box
+            display="flex"
+            flexDirection="row"
+            bg="background.primary"
+            px={2}
+            py={1}
+            borderRadius={100}
+            boxShadow="secondaryShadow"
+            mr={4}
+          >
             <Box transform="scale(0.8)">
-              <IconButton 
-                onClick={signOut} 
-                icon={<FaSignOutAlt color={themCtx?.theme.colors.icons.primaryColor} />} 
+              <IconButton
+                onClick={signOut}
+                icon={
+                  <FaSignOutAlt
+                    color={themCtx?.theme.colors.icons.primaryColor}
+                  />
+                }
                 boxShadow="unset"
                 aria-label="sign out"
                 variant="navButtons"
