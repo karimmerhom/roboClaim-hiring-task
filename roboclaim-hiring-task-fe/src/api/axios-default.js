b5ea7bc5c5794d1593c5574d7489abf11da90d62
karimmerhom <karimebrahim328@gmail.com>
2024-11-22 14:00:44 +0200

    changing be link

--- roboclaim-hiring-task-fe/src/api/axios-default.js
+++ roboclaim-hiring-task-fe/src/api/axios-default.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = "http://165.227.171.60:5000/";
 axios.defaults.headers.common["Cache-Control"] = "no-cache";
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem("access_token");
