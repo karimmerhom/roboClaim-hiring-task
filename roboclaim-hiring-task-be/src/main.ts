b3ae8458f6e0f5cd5b4518bce7632a85ca8297d1
karimmerhom <karimebrahim328@gmail.com>
2024-11-22 14:12:47 +0200

    cors

--- roboclaim-hiring-task-be/src/main.ts
+++ roboclaim-hiring-task-be/src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000','http://165.227.171.60:3000'],
-    credentials: true,                
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    origin: ['http://localhost:3000', 'http://165.227.171.60:3000'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   await app.listen(5000); 
